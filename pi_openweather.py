34194357b122ad2093d84243649090193ebb194b
palfiistvan68 <palfiistvan68@gmail.com>
2025-04-24 16:10:52 +0200

    Update pi_openweather.py

--- pi_openweather.py
+++ pi_openweather.py
@@ -8,7 +8,7 @@ api_key = st.secrets["openweather"]["api_key"]
 # Set Cache-s:
 @st.cache_data
 def get_weather(city):
-    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
     response = requests.get(url)
     if response.status_code == 200:
         return response.json()
